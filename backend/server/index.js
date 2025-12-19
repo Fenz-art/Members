65cd786d42a7c9b86f25799880484e03ea7b751d
Fenz-art <kushangatwork@gmail.com>
2025-12-19 06:16:08 +0530

    patch updates

--- backend/server/index.js
+++ backend/server/index.js
@@ -15,3 +15,11 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Backend running on port", PORT);
 });
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error"
+  });
+});
